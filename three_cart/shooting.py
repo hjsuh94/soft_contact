71a58099352a915905c64c0445c32a664ddf4849
Terry Suh <hjsuh94@gmail.com>
2020-04-30 00:19:49 -0400

    Updated sim

--- three_cart/shooting.py
+++ three_cart/shooting.py
@@ -30,20 +30,22 @@ class CartSimTensorized(sim.CartSimulator):
         # forward integrate to obtain a target state
         for t in range(self.timesteps):
             # Compute contact forces first
-            lambda1 = torch.nn.ReLU()(-self.params['k'] * (self.states[2,t] - self.states[0,t] - self.params['d']))
-            lambda2 = torch.nn.ReLU()(-self.params['k'] * (self.states[4,t] - self.states[2,t] - self.params['d']))
+            lambda1 = torch.nn.ReLU()(-self.params['k'] * (
+                self.states[1,t] - self.states[0,t] - self.params['d']))
+            lambda2 = torch.nn.ReLU()(-self.params['k'] * (
+                self.states[2,t] - self.states[1,t] - self.params['d']))
             self.forces[:,t] = torch.stack([lambda1, lambda2]).double().detach()
 
             # Propagate velocities ### EXAMINE HERE IF LAMBDA1 CAN BE USED
-            self.states[1,t+1] = self.states[1,t] + self.params['h'] * \
-                                (-self.params['c'] * self.states[1,t] - lambda1 + inputs[0,t])
             self.states[3,t+1] = self.states[3,t] + self.params['h'] * \
-                                (-self.params['c'] * self.states[3,t] + lambda1 - lambda2)
+                                (-self.params['c'] * self.states[3,t] - lambda1 + inputs[0,t])
+            self.states[4,t+1] = self.states[4,t] + self.params['h'] * \
+                                (-self.params['c'] * self.states[4,t] + lambda1 - lambda2)
             self.states[5,t+1] = self.states[5,t] + self.params['h'] * \
                                 (-self.params['c'] * self.states[5,t] + lambda2 + inputs[1,t])
 
             # Propagate positions at t with velocities at t+1
-            self.states[[0,2,4],t+1] = self.states[[0,2,4],t] + self.params['h'] * self.states[[1,3,5],t+1]
+            self.states[0:3,t+1] = self.states[0:3,t] + self.params['h'] * self.states[3:6,t+1]
             
         return self.states[:, -1] 
 
@@ -67,21 +69,21 @@ def simulate(simulator, init_state, inputs):
 
 # hyper params 
 timesteps = 10000
-init_state = np.array([100, 0, 300, 0, 400, 0]).astype(np.double)
+init_state = np.array([1, 3, 4, 0, 0, 0]).astype(np.double)
 inputs = np.array([[10, -10] for i in range(timesteps)]).T
 params = {'m': 1.,  # mass
           'c': 3.0,  # viscosity
           'k': 100.0,  # elasticity
-          'd': 80,    # cart length
-          'w': 50,    # cart height (just for visualization) 
+          'd': 0.5,    # cart length
+          'w': 0.5,    # cart height (just for visualization) 
           'h': 0.01,   # time step
           't': 0.0}   # sleep time before redrawing on canvas
 
 
 # set initial initial state, initial guess input traj, and goal state
-init_state = np.array([100, 0, 300, 0, 400, 0]).astype(np.double)
+init_state = np.array([1, 3, 4, 0, 0, 0]).astype(np.double)
 guess_inputs = np.array([[0, 0] for i in range(timesteps)]).T 
-center_cart_goal_x = 267
+center_cart_goal_x = 1.5
 goal_state = torch.tensor([center_cart_goal_x - params['d'], 0,
 						   center_cart_goal_x,               0,
 						   center_cart_goal_x + params['d'], 0])
@@ -116,11 +118,11 @@ for epoch in range(epochs):
     losses[epoch] = loss.data.numpy()
     if True :
         print('loss: ', '{:.3f}'.format(loss.data.numpy()))
-
 time_elapsed = time.time() - since
 print('Training takes in {:.0f}m {:.0f}s'.format(time_elapsed // 60,
                                                 time_elapsed % 60))
 
+guess_inputs = guess_inputs.cpu()
 
 
 # visualize simulation
@@ -128,6 +130,7 @@ C = sim.CartSimulator(gui=True, video=True)
 for key, value in params.items():
     C.set_parameters(key, value)
 _, _ = simulate(C, init_state, guess_inputs)
+C.save_video("new_sim.avi")
 
 
 # now plot the trajectory
